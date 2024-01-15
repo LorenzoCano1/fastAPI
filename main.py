76ad00c531a668a6dd585cff363b6917a5ea8cee
Cano <lcano@ausol.com.ar>
2024-01-15 15:47:38 -0300

    Creando PUT

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 ﻿from http.client import HTTPException
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Creo la app para despues ejecutar el comando uvicron main:app(depende el nombre de la variable) --reload(Para que se actualice solo cuando haces algun cambio)
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool,None] = None
 
 
 # Ruta raiz
@@ -29,4 +35,9 @@ def calcular(num1: float, num2: float):
         return {'suma': resultado}
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error en la operación: {e}")
+    
+
+@app.put('/items/{item_id}')
+def update_item(item_id: int, item: Item):
+    return {'item_name': item.name, 'item_id': item_id, 'item_price' : item.price}
 
